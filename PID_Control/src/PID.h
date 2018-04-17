4e34ec67fabda49d91b5651211de3a5e27720a0e
vamsikkv <kandadavamsi56@gmail.com>
2018-04-18 01:54:39 +0530

    Add files via upload

---
+++ PID_Control/src/PID.h
@@ -0,0 +1,56 @@
+#ifndef PID_H
+#define PID_H
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class PID {
+public:
+  /*
+  * Errors
+  */
+  double p_error;
+  double i_error;
+  double d_error;
+
+  /*
+  * Coefficients
+  */ 
+  double Kp;
+  double Ki;
+  double Kd;
+
+  vector<double> p;
+  vector<double> d;
+  int twiddle_for_steps;
+  double prev_cte;
+  int steps = 0;
+  double best_error;
+
+  /*
+  * Constructor
+  */
+  PID();
+
+  /*
+  * Destructor.
+  */
+  virtual ~PID();
+
+  /*
+  * Initialize PID.
+  */
+  void Init(double Kp, double Ki, double Kd);
+
+  /*
+  * Update the PID error variables given cross track error.
+  */
+  void UpdateError(double cte);
+
+  /*
+  * Calculate the total PID error.
+  */
+  double TotalError();
+};
+
+#endif /* PID_H */
